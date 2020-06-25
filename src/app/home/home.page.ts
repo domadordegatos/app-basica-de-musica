a88fd7a064e9d37d7a6bc74acde6b7a288138f7f
Neyder Neme <neyderflashh@gmail.com>
2020-06-25 10:10:07 -0500

    version final

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,18 @@ export class HomePage {
 
   constructor() {}
 
+  instrumentos = [
+    {nombre: 'Flauta', imagen:'assets/imagenes/flauta.png', audio:'assets/sonidos/flauta.mp3'},
+    {nombre: 'PLatillos', imagen:'assets/imagenes/platillos.png', audio:'assets/sonidos/platillos.mp3'},
+    {nombre: 'Bombo1', imagen:'assets/imagenes/bombo1.png', audio:'assets/sonidos/bombo1.mp3'},
+    {nombre: 'Bombo2', imagen:'assets/imagenes/bombo2.png', audio:'assets/sonidos/bombo2.mp3'},
+  ]
+
+  soundPlay(item:any){
+    console.log(item);
+    const sonido = new Audio();
+    sonido.src = item.audio;
+    sonido.load();
+    sonido.play();
+  }
 }
